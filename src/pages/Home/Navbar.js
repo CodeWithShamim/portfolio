bf36f78870a15d72cd9a173a23a4a784d875add0
codewithshamim <shamimislamonline@gmail.com>
2022-06-09 16:40:31 +0600

    nabvar udpated

--- src/pages/Home/Navbar.js
+++ src/pages/Home/Navbar.js
@@ -7,27 +7,25 @@ const Navbar = () => {
     <div>
       <nav className="w-full flex items-center justify-between px-16 py-6 z-50">
         <div>
-          <h1 className="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-accent ">
-            <Link to="/" className="shadow-zinc-500 shadow-xl">
-              SHAMIM ISLAM
-            </Link>
+          <h1 className="font-bold tracking-wide text-xl text-transparent bg-clip-text bg-gradient-to-r from-secondary to-accent hover:from-pink-400 hover:scale-105 transition-all">
+            <Link to="/">SHAMIM ISLAM</Link>
           </h1>
         </div>
 
-        <div className="grid grid-flow-col gap-3">
-          <h1>
+        <div className="grid grid-flow-col gap-3 uppercase text-sm font-bold tracking-widest">
+          <h1 className="hover:text-pink-400 hover:scale-105 transition-all">
             <Link to="/">About</Link>
           </h1>
-          <h1>
+          <h1 className="hover:text-pink-400 hover:scale-105 transition-all">
             <Link to="/">Projects</Link>
           </h1>
-          <h1>
+          <h1 className="hover:text-pink-400 hover:scale-105 transition-all">
             <Link to="/">Skills</Link>
           </h1>
-          <h1>
+          <h1 className="hover:text-pink-400 hover:scale-105 transition-all">
             <Link to="/">Contact</Link>
           </h1>
-          <h1>
+          <h1 className="hover:text-pink-400 hover:scale-105 transition-all">
             <Link to="/">Testimonials</Link>
           </h1>
         </div>
