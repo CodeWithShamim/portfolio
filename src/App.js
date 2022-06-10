da2ab4e9979f94358dc27e5e83f89cb9453cc9ee
codewithshamim <shamimislamonline@gmail.com>
2022-06-10 15:45:05 +0600

    preloader added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Home from "./pages/Home/Home";
+import BarLoader from "react-spinners/BarLoader";
+import DotLoader from "react-spinners/DotLoader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  // ----pre loader----
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-screen">
+        <p className="text-secondary animate-bounce ease-linear text-2xl font-bold">
+          SHAMIM ISLAM
+        </p>
+        <BarLoader color="#ffffff" loading={loading} height={5} width={180} />
+        <DotLoader color="white" loading={loading} height={10} width={180} />
+      </div>
+    );
+  }
+
   return (
     <div className="App text-secondary font-serif">
       <Home />
