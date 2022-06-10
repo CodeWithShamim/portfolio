9014d8b2512ae42caa0fae1dbcf3bdab7dc3155d
codewithshamim <shamimislamonline@gmail.com>
2022-06-10 23:16:28 +0600

    style changes for projects page

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -9,7 +9,7 @@ const Projects = () => {
       <h1 className="text-5xl">Recent Projects</h1>
 
       {/* -----projects----- */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5  pt-8 md:pt-12 lg:pt-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5  pt-8 md:pt-12 lg:pt-16">
         {/* -------------------------1--------------------------- */}
         <div className="bg-secondary rounded-2xl relative">
           <p>
@@ -26,21 +26,28 @@ const Projects = () => {
             </a>
           </p>
 
+          {/* see details  */}
+          <div>
+            <button className="btn btn-primary text-secondary font-semibold animate-bounce">
+              Details
+            </button>
+          </div>
+
           <h1 className="my-4 text-xl font-bold text-primary">
             Ryan refrigerator instrument (Full Stack)
           </h1>
 
           {/* technologies used */}
           <div className="px-2 pb-10 grid grid-cols-4 gap-1 ">
-            <span className="bg-accent rounded-full px-2">React</span>
-            <span className="bg-accent rounded-full px-2">TailwindCSS</span>
-            <span className="bg-accent rounded-full px-2">Daisy UI</span>
-            <span className="bg-accent rounded-full px-2">Stripe</span>
-            <span className="bg-accent rounded-full px-2">JWT</span>
-            <span className="bg-accent rounded-full px-2">Express.js</span>
-            <span className="bg-accent rounded-full px-2">Node.js</span>
-            <span className="bg-accent rounded-full px-2">MongoDB</span>
-            <span className="bg-accent rounded-full px-2">Firebase</span>
+            <span className="bg-primary rounded-full px-2">React</span>
+            <span className="bg-primary rounded-full px-2">TailwindCSS</span>
+            <span className="bg-primary rounded-full px-2">Daisy UI</span>
+            <span className="bg-primary rounded-full px-2">Stripe</span>
+            <span className="bg-primary rounded-full px-2">JWT</span>
+            <span className="bg-primary rounded-full px-2">Express.js</span>
+            <span className="bg-primary rounded-full px-2">Node.js</span>
+            <span className="bg-primary rounded-full px-2">MongoDB</span>
+            <span className="bg-primary rounded-full px-2">Firebase</span>
           </div>
           {/* code link*/}
           <div className="font-bold grid grid-flow-col bg-secondary text-blue-500 rounded-2xl py-2 absolute bottom-0 left-0 right-0">
@@ -89,19 +96,27 @@ const Projects = () => {
               />
             </a>
           </p>
+
+          {/* see details  */}
+          <div>
+            <button className="btn btn-primary text-secondary font-semibold animate-bounce">
+              Details
+            </button>
+          </div>
+
           <h1 className="my-4 text-xl font-bold text-primary">
             Ryan books store (Full Stack)
           </h1>
 
           {/* technologies used */}
           <div className="px-2 pb-10 grid grid-cols-4 gap-1 ">
-            <span className="bg-accent rounded-full px-2">React</span>
-            <span className="bg-accent rounded-full px-2">Bootstrap</span>
-            <span className="bg-accent rounded-full px-2">JWT</span>
-            <span className="bg-accent rounded-full px-2">Express.js</span>
-            <span className="bg-accent rounded-full px-2">Node.js</span>
-            <span className="bg-accent rounded-full px-2">MongoDB</span>
-            <span className="bg-accent rounded-full px-2">Firebase</span>
+            <span className="bg-primary rounded-full px-2">React</span>
+            <span className="bg-primary rounded-full px-2">Bootstrap</span>
+            <span className="bg-primary rounded-full px-2">JWT</span>
+            <span className="bg-primary rounded-full px-2">Express.js</span>
+            <span className="bg-primary rounded-full px-2">Node.js</span>
+            <span className="bg-primary rounded-full px-2">MongoDB</span>
+            <span className="bg-primary rounded-full px-2">Firebase</span>
           </div>
           {/* code link*/}
           <div className="font-bold grid grid-flow-col bg-secondary text-blue-500 rounded-2xl py-2 absolute bottom-0 left-0 right-0">
@@ -150,15 +165,23 @@ const Projects = () => {
               />
             </a>
           </p>
+
+          {/* see details  */}
+          <div>
+            <button className="btn btn-primary text-secondary font-semibold animate-bounce">
+              Details
+            </button>
+          </div>
+
           <h1 className="my-4 text-xl font-bold text-primary">
             Eyun fitness (Gym website)
           </h1>
 
           {/* technologies used */}
           <div className="px-2 pb-10 grid grid-cols-3 gap-1">
-            <span className="bg-accent rounded-full px-2">React</span>
-            <span className="bg-accent rounded-full px-2">Bootstrap</span>
-            <span className="bg-accent rounded-full px-2">Firebase</span>
+            <span className="bg-primary rounded-full px-2">React</span>
+            <span className="bg-primary rounded-full px-2">Bootstrap</span>
+            <span className="bg-primary rounded-full px-2">Firebase</span>
           </div>
           {/* code link*/}
           <div className="font-bold grid grid-flow-col bg-secondary text-blue-500 rounded-2xl py-2 absolute bottom-0 left-0 right-0">
