3d072f5d7750be83b4658456214b38a632c7cf04
codewithshamim <shamimislamonline@gmail.com>
2022-06-13 14:16:16 +0600

    projects updated

--- src/pages/Projects/ProjectItem.js
+++ src/pages/Projects/ProjectItem.js
@@ -3,11 +3,14 @@ import React from "react";
 const ProjectItem = ({ project }) => {
   const { name, img } = project;
   return (
-    <div>
+    <div className="shadow w-ful">
       <div>
-        <img src={img} alt={name} />
+        <img className="w-full" src={img} alt={name} />
       </div>
-      <h1>{name}</h1>
+      <h1 className="py-3">{name}</h1>
+      <button className="btn btn-xs btn-error w-full text-secondary font-bold">
+        Details
+      </button>
     </div>
   );
 };
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -10,9 +10,9 @@ const Projects = () => {
   }, []);
 
   return (
-    <div id="projects" className="px-4 md:px-10 py-12">
-      <h1 className="text-5xl">Recent Projects</h1>
-      <div>
+    <div id="projects" className="px-4 md:px-10 py-20">
+      <h1 className="text-3xl">Recent Projects</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-6">
         {projects?.map((project) => (
           <ProjectItem key={project.id} project={project}></ProjectItem>
         ))}
