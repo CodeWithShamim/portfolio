88cb1fb6fca0df6a66b4a7fa50cf02470596fca3
codewithshamim <shamimislamonline@gmail.com>
2023-08-03 15:00:20 +0600

    updated

--- src/components/MessangerChatBot.tsx
+++ src/components/MessangerChatBot.tsx
@@ -5,8 +5,8 @@ const MessangerChatBot = () => {
   return (
     <div className="bg-zinc-400">
       <MessengerCustomerChat
-        pageId={process.env.FB_PAGE_ID}
-        appId={process.env.FB_APP_ID}
+        pageId="104414818972089"
+        appId="2364713910333982"
         themeColor="#F200F2"
       />
     </div>
