2f229adddf326744aad4d8506554fbf87808bf6d
codewithshamim <shamimislamonline@gmail.com>
2023-08-13 20:31:43 +0600

    webview loader added

--- android/App.tsx
+++ android/App.tsx
@@ -1,18 +1,56 @@
 /* eslint-disable react-native/no-inline-styles */
-import {StatusBar} from 'react-native';
-import React from 'react';
+import {
+  ActivityIndicator,
+  Dimensions,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 import WebView from 'react-native-webview';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const {width, height} = Dimensions.get('window');
+
   return (
     <>
       <StatusBar backgroundColor="#FF33FF" />
+
+      {isLoading && (
+        <View style={[styles.loaderContainer, {width, height}]}>
+          <Text style={styles.loader}>Loading...</Text>
+          <ActivityIndicator color="#FF33FF" />
+        </View>
+      )}
+
       <WebView
         source={{uri: 'https://shamim-islam.vercel.app/'}}
         style={{flex: 1}}
+        onLoadEnd={syntheticEvent => {
+          const {nativeEvent} = syntheticEvent;
+          setIsLoading(nativeEvent.loading);
+        }}
       />
     </>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  loaderContainer: {
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  loader: {
+    fontWeight: '700',
+    fontSize: 16,
+    color: '#FF33FF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+});
